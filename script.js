fcaa8ccb1e99fe0edf47bbacf27f1caac1871a07
carloshg12 <carhergar@alu.edu.gva.es>
2024-02-01 16:47:12 +0100

    Web finished

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Funcionalidades adicionales pueden ser añadidas aquí
+    console.log('Bienvenido a CJI Software - Experiencia y Calidad en Software');
+});
